4042969f42b5da88658f10a72ec6f001e339ac21
Peta <peta@MacBook-Pro-de-Peta.local>
2024-02-17 16:38:47 +0000

    created three Employee objects

--- src/main/java/com/mycompany/employeetest/EmployeeTest.java
+++ src/main/java/com/mycompany/employeetest/EmployeeTest.java
@@ -12,6 +12,11 @@ package com.mycompany.employeetest;
 public class EmployeeTest {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
+        // Creates 3 Employee objects
+        Employee emp1 = new Employee("Joe Bloggs", "jb@gmail.com");
+        Employee emp2 = new Employee("Ann Banana", "ab@gmail.com");
+        Employee emp3 = new Employee("Tom Thumb", "tt@gmail.com");
+        
     }
 }
