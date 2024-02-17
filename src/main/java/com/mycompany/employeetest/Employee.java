110110b69cf29c291e5e99d07d92bd13d4e82fcb
Peta <peta@MacBook-Pro-de-Peta.local>
2024-02-17 16:37:30 +0000

    setting the email field to a new value

--- src/main/java/com/mycompany/employeetest/Employee.java
+++ src/main/java/com/mycompany/employeetest/Employee.java
@@ -36,6 +36,11 @@ public class Employee {
         return empNum;
     }
     
+    // To set the email field to a new value passed as a parameter.  ///////////The value must not be accepted if its length is 3 or less.
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
     // Static field: nextEmpNum, of type int and initialised to 1, ??????which initialises empNum in the constructor???????
     static int nextEmpNum = 1;
 }
